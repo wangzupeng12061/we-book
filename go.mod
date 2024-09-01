7269011c72cf9ffaf22be3f3c2a9df7dbe59beed
wangzupeng <wangzupeng12061@gmail.com>
2024-09-01 13:53:06 +0800

    feat: 校验

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/wangzupeng12061/we-book
 
 go 1.22.5
 
-require github.com/gin-gonic/gin v1.10.0
+require (
+	github.com/dlclark/regexp2 v1.11.4
+	github.com/gin-gonic/gin v1.10.0
+)
 
 require (
 	github.com/bytedance/sonic v1.11.6 // indirect
--- internal/web/user.go
+++ internal/web/user.go
@@ -1,13 +1,28 @@
 package web
 
 import (
+	regexp "github.com/dlclark/regexp2"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 type UserHandler struct {
+	emailRexExp    *regexp.Regexp
+	passwordRexExp *regexp.Regexp
 }
 
+func NewUserHandler() *UserHandler {
+	const (
+		emailRegexPattern = "^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$"
+		// 和上面比起来，用 ` 看起来就比较清爽
+		passwordRegexPattern = `^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$`
+	)
+	return &UserHandler{
+		emailRexExp:    regexp.MustCompile(emailRegexPattern, regexp.None),
+		passwordRexExp: regexp.MustCompile(passwordRegexPattern, regexp.None),
+	}
+
+}
 func (u UserHandler) RegisterRoutes(server *gin.Engine) {
 	ug := server.Group("/users")
 	ug.GET("/profile", u.Profile)
@@ -16,8 +31,36 @@ func (u UserHandler) RegisterRoutes(server *gin.Engine) {
 	ug.POST("/signup", u.SignUp)
 }
 func (u *UserHandler) SignUp(ctx *gin.Context) {
-	ctx.String(http.StatusOK, "hello, signup")
+	type SignUpReq struct {
+		Email           string `json:"email"`
+		Password        string `json:"password"`
+		ConfirmPassword string `json:"confirmPassword"`
+	}
+	var req SignUpReq
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
+	matched, err := u.emailRexExp.MatchString(req.Email)
+	if err != nil {
+		ctx.JSON(http.StatusOK, gin.H{"message": "system error"})
+		return
+	}
+	if !matched {
+		ctx.JSON(http.StatusOK, gin.H{"message": "email format error"})
+		return
+	}
+	matched, err = u.passwordRexExp.MatchString(req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusOK, gin.H{"message": "system error"})
+		return
+	}
+	if !matched {
+		ctx.JSON(http.StatusOK, gin.H{"message": "password format error"})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
 }
 func (u UserHandler) Login(ctx *gin.Context) {
 	ctx.String(http.StatusOK, "hello, login")
