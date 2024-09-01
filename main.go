2eef033eba277c949c459d533690dda82c4680ea
wangzupeng <wangzupeng12061@gmail.com>
2024-09-01 18:02:33 +0800

    feat:支持跨域请求

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/wangzupeng12061/we-book/internal/web"
+	"strings"
+	"time"
 )
 
 func main() {
 	server := gin.Default()
+	server.Use(cors.New(cors.Config{
+		//AllowOrigins: []string{"http://localhost:3000"},
+		//AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "DELETE"},
+		AllowHeaders: []string{"authorization", "content-type"},
+		//ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				return true
+			}
+			return strings.Contains(origin, "example.com")
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 	u := &web.UserHandler{}
 	u.RegisterRoutes(server)
 	server.Run(":8080")
